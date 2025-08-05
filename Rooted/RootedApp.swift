ec39fcedf9d41ef506da73ef30e59545220fe8a6
Asher Greer <asher@ashergreer.com>
2025-08-04 20:41:15 -0600

    Initial Commit

---
+++ Rooted/RootedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RootedApp.swift
+//  Rooted
+//
+//  Created by Asher Greer on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct RootedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RootedTests/RootedTests.swift
@@ -0,0 +1,17 @@
+//
+//  RootedTests.swift
+//  RootedTests
+//
+//  Created by Asher Greer on 8/4/25.
+//
+
+import Testing
+@testable import Rooted
+
+struct RootedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
